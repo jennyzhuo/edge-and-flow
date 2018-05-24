d52b9aa7e25f3a1869a6b52ced7d94c86b229e47
Patrick Jang <patrick.jang@dynamotechnologies.com>
2018-05-23 21:28:25 -0400

    create navbar component

---
+++ src/modules/home/NavBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const NavBar = () => (
+  <div className="menu p-3">
+    <div className="btn-group btn-group-sm" role="group">
+      <button type="button" className="btn btn-secondary px-3">01 HOME</button>
+      <button type="button" className="btn btn-secondary px-3">02 PERSONAL</button>
+      <button type="button" className="btn btn-secondary px-3">03 INTERESTS</button>
+      <button type="button" className="btn btn-secondary px-3">04 CONNECT</button>
+    </div>
+  </div>
+)
--- src/modules/home/index.js
+++ src/modules/home/index.js
@@ -1,6 +1,6 @@
-
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { NavBar } from './NavBar';
 
 @connect(() => {})
 
@@ -23,14 +23,15 @@ class Home extends Component {
 	         	from the cross-pollination of each other's enthusiasm.
 	         </p>
 	      </div>
-	      <div className="menu p-3">
-	      	<div className="btn-group btn-group-sm" role="group">
-			    <button type="button" className="btn btn-secondary px-3">01 HOME</button>
-			    <button type="button" className="btn btn-secondary px-3">02 PERSONAL</button>
-			    <button type="button" className="btn btn-secondary px-3">03 INTERESTS</button>
-			    <button type="button" className="btn btn-secondary px-3">04 CONNECT</button>
-			  </div>
-	      </div>
+				<NavBar />
+        {/*<div className="menu p-3">*/}
+	      	{/*<div className="btn-group btn-group-sm" role="group">*/}
+			    {/*<button type="button" className="btn btn-secondary px-3">01 HOME</button>*/}
+			    {/*<button type="button" className="btn btn-secondary px-3">02 PERSONAL</button>*/}
+			    {/*<button type="button" className="btn btn-secondary px-3">03 INTERESTS</button>*/}
+			    {/*<button type="button" className="btn btn-secondary px-3">04 CONNECT</button>*/}
+			  {/*</div>*/}
+        {/*</div>*/}
       </div>
     );
   }
