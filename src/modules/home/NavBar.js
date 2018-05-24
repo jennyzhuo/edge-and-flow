caf777c2ad07f310a0fe2200f1723f1f0268fe59
Patrick Jang <patrick.jang@dynamotechnologies.com>
2018-05-23 22:23:51 -0400

    actually fix routing

--- src/modules/home/NavBar.js
+++ src/modules/home/NavBar.js
@@ -4,16 +4,16 @@ import { Link } from "react-router-dom";
 export default () => (
   <div className="menu p-3">
     <div className="btn-group btn-group-sm" role="group">
-      <Link to="/">
+      <Link to={`/home`}>
         <button type="button" className="btn btn-secondary px-3">01 HOME</button>
       </Link>
-      <Link to="/personal">
+      <Link to={`/home/personal`}>
         <button type="button" className="btn btn-secondary px-3">02 PERSONAL</button>
       </Link>
-      <Link to="/interests">
+      <Link to={`/home/interests`}>
         <button type="button" className="btn btn-secondary px-3">03 INTERESTS</button>
       </Link>
-      <Link to="/connect">
+      <Link to={`/home/connect`}>
         <button type="button" className="btn btn-secondary px-3">04 CONNECT</button>
       </Link>
     </div>
--- src/modules/home/index.js
+++ src/modules/home/index.js
@@ -5,17 +5,16 @@ import Personal from './personal';
 import Skills from './skills';
 import Connect from './connect';
 
-import { Router, Switch, Route, Redirect } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 
 export default ({ history, match }) => (
   <div className="left-side home p-5">
-    {/*<Route exact path={`${match.url}`} component={Intro} />*/}
-		<Route exact path={`/`} component={Intro} />
-    <Route path={`/personal`} component={Personal} />
-    <Route path={`/interests`} component={Skills} />
-    <Route path={`/connect`} component={Connect} />
+		<Route path={`${match.url}`} component={Intro} />
+    <Route path={`${match.url}/personal`} component={Personal} />
+    <Route path={`${match.url}/interests`} component={Skills} />
+    <Route path={`${match.url}/connect`} component={Connect} />
 
-    <NavBar history={history} />
+    <NavBar />
   </div>
 )
 
