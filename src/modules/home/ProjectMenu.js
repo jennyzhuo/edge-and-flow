1925931e5254b1b8af95a521b656fd58128acbf7
Patrick Jang <patrick.jang@dynamotechnologies.com>
2018-05-23 23:17:43 -0400

    create project menu component

---
+++ src/modules/home/ProjectMenu.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default () => (
+  <div className="right-side projects p-5">
+    <div className="menu pl-5">
+      <h3 className="pb-3">+ RECENT WORKS</h3>
+      <div className="pt-5 btn-group-vertical btn-group-sm" role="group">
+        <button type="button" className="btn px-3 py-2"><b>01  </b>DYNAMO</button>
+        <button type="button" className="btn px-3 py-2"><b>02  </b>CAPTIS</button>
+        <button type="button" className="btn px-3 py-2"><b>03  </b>BURRIS</button>
+        <button type="button" className="btn px-3 py-2"><b>04  </b>HACKATHON</button>
+        <button type="button" className="btn px-3 py-2"><b>05  </b>NORASACK DESIGN</button>
+        <button type="button" className="btn px-3 py-2"><b>06  </b>OTHER</button>
+        <button type="button" className="btn px-3 py-2"><b>←  </b>BACK</button>
+      </div>
+    </div>
+  </div>
+)
--- src/modules/home/index.js
+++ src/modules/home/index.js
@@ -4,16 +4,20 @@ import Intro from './intro';
 import Personal from './personal';
 import Skills from './skills';
 import Connect from './connect';
+import ProjectMenu from './ProjectMenu';
 
 import { Route } from 'react-router-dom'
 
 export default ({ history, match }) => (
-  <div className="left-side home p-5">
-		<Route exact path={`${match.url}`} component={Intro} />
-    <Route path={`${match.url}/personal`} component={Personal} />
-    <Route path={`${match.url}/interests`} component={Skills} />
-    <Route path={`${match.url}/connect`} component={Connect} />
-    <NavBar />
-  </div>
+  [
+    <div className="left-side home p-5">
+      <Route exact path={`${match.url}`} component={Intro} />
+      <Route path={`${match.url}/personal`} component={Personal} />
+      <Route path={`${match.url}/interests`} component={Skills} />
+      <Route path={`${match.url}/connect`} component={Connect} />
+      <NavBar />
+    </div>,
+    <ProjectMenu/>
+  ]
 )
 
