276ece96723f887944cf7b7308d27be1635aa5fd
Patrick Jang <patrick.jang@dynamotechnologies.com>
2018-05-23 21:32:39 -0400

    turn home into a stateless component

--- src/modules/home/NavBar.js
+++ src/modules/home/NavBar.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-export const NavBar = () => (
+export default ({ history }) => (
   <div className="menu p-3">
     <div className="btn-group btn-group-sm" role="group">
-      <button type="button" className="btn btn-secondary px-3">01 HOME</button>
+      <button type="button" onClick={() => {}} className="btn btn-secondary px-3">01 HOME</button>
       <button type="button" className="btn btn-secondary px-3">02 PERSONAL</button>
       <button type="button" className="btn btn-secondary px-3">03 INTERESTS</button>
       <button type="button" className="btn btn-secondary px-3">04 CONNECT</button>
