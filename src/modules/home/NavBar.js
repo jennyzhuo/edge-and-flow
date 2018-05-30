3540163ba57749208aaa7a2306dc7e17a7d52c59
Patrick Jang <patrick.jang@dynamotechnologies.com>
2018-05-29 21:09:48 -0400

    more cleanup

--- src/modules/home/NavBar.js
+++ src/modules/home/NavBar.js
@@ -1,9 +1,8 @@
-import React, { Component } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import { aboutToAbout } from "../transitions";
 
 export default ({ location }) => {
-  console.log("location = ", location);
   return (
     <div className="menu mt-5 pt-5">
       <div className="btn-group btn-group-sm" role="group">
@@ -23,12 +22,3 @@ export default ({ location }) => {
     </div>
   )
 }
-
-// class NavBar extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//
-//     }
-//   }
-// }
